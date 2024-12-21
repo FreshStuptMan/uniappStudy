2af28177a4db2774a0540baf8a9e0d6036a140ad
FreshStupidMan <1367213168@qq.com>
2024-12-21 16:20:04 +0800

    商品列表页完成

---
+++ components/my-goods/my-goods.vue
@@ -0,0 +1,61 @@
+<template>
+	<view>
+		<view class="goods-item">
+			<view class="left-image">
+				<image class="goods-image" :src="item.goods_small_logo || defaultPic"></image>
+			</view>
+			<view class="right-info">
+				<view class="goods-name">{{item.goods_name}}</view>
+				<view class="goods-price">￥{{item.goods_price | tofixed}}</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		props: {
+			item: {
+				type: Object,
+				defaul: {}
+			}
+		},
+		filters: {
+		  // 把数字处理为带两位小数点的数字
+		  tofixed(num) {
+		    return Number(num).toFixed(2)
+		  }
+		},
+		data() {
+			return {
+				defaultPic: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png'
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+.goods-item {
+	display: flex;
+	flex-direction: row;
+	padding: 10px 5px;
+	border-bottom: 1px solid #f0f0f0;
+}
+.goods-image {
+	width: 100px;
+	height: 100px;
+}
+.right-info {
+	padding-left: 5px;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+}
+.goods-name {
+	font-size: 14px;
+}
+.goods-price {
+	font-weight: bolder;
+	color: #C00000;
+}
+</style>
--- subpackages/goods_list/goods_list.vue
+++ subpackages/goods_list/goods_list.vue
@@ -1,6 +1,10 @@
 <template>
 	<view>
-		
+		<view class="goods-list">
+			<view @click="gotoGoodsDetail(item)" v-for="item in goodsList" :item="item" :key="item.goods_id">
+				<my-goods :item="item"></my-goods>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,12 +12,75 @@
 	export default {
 		data() {
 			return {
-				
+				webUrlInfo: 'https://api-hmugo-web.itheima.net',
+				goodsList: [],
+				query: '',
+				cid: '',
+				pageSize: 10,
+				pageNum: 1,
+				total: 0,
+				isLoading: false
 			};
+		},
+		methods: {
+			getGoodsList(callback) {
+				this.isLoading = true
+				uni.request({
+					url: `${this.webUrlInfo}/api/public/v1/goods/search`,
+					method: 'GET',
+					data: {
+						query: this.query,
+						cid: this.cid,
+						// 页码值
+						pagenum: this.pageNum,
+						// 每页显示多少条数据
+						pagesize: this.pageSize
+					},
+					success: (res) => {
+						this.goodsList = [...this.goodsList,...res.data.message.goods]
+						this.total = res.data.message.total
+						console.log(this.goodsList)
+					},
+					complete: () => {
+						this.isLoading = false
+						callback && callback()
+					}
+				})
+			},
+			gotoGoodsDetail (item) {
+				uni.navigateTo({
+					url: '/subpackages/good_detail/good_detail?goods_id=' + item.goods_id
+				})
+			}
+		},
+		onLoad(option) {
+			if (option.query) {
+				this.query = option.query
+			}
+			if (option.cid) {
+				this.cid = option.cid
+			}
+			this.getGoodsList()
+		},
+		onReachBottom () {
+			if ((this.pageSize * this.pageNum) >= this.total) {
+				return
+			}
+			if (this.isLoading) {
+				return
+			}
+			this.pageNum++
+			this.getGoodsList()
+		},
+		onPullDownRefresh () {
+			this.pageNum = 1
+			this.isLoading = false
+			this.goodsList = []
+			this.total = 0
+			this.getGoodsList(() => uni.stopPullDownRefresh())
 		}
 	}
 </script>
 
 <style lang="scss">
-
 </style>
